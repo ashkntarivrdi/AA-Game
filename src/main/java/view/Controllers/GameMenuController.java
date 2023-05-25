9e73a740d17f7749f5490d425ed0d2d945b9cbe0
AshkanTariverdi <ashkantariverdi82@gmail.com>
2023-05-25 16:43:28 +0330

    next track feature added

--- src/main/java/view/Controllers/GameMenuController.java
+++ src/main/java/view/Controllers/GameMenuController.java
@@ -3,13 +3,23 @@ package view.Controllers;
 import controller.GameController;
 import controller.SettingController;
 import enums.Phase;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Popup;
 import model.CurrentGame;
 import view.Menus.GameMenu;
 import view.Menus.LoginMenu;
 import view.Menus.MainMenu;
 
 import java.util.Set;
+
+import static view.Menus.GameMenu.mediaPlayer;
 
 public class GameMenuController {
     GameController gameController = new GameController();
@@ -25,22 +35,49 @@ public class GameMenuController {
     }
 
     public void showKeyBinds(MouseEvent mouseEvent) {
-        //TODO
+        Label text = new Label("Shoot: " + CurrentGame.getShootKey() + "\nFreeze: " + CurrentGame.getFreezeKey() +
+                "\nMove Right: " + CurrentGame.getRightKey() + "\nMove Left: " + CurrentGame.getLeftKey() +
+                "\nPause: " + KeyCode.ESCAPE.getName());
+        Font font = Font.font("serif", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 14);
+        text.setFont(font);
+        text.setTranslateX(20);
+        text.setTranslateY(400);
+
+        Popup popup = new Popup();
+        popup.getContent().add(text);
+
+        popup.setAutoHide(true);
+        if (!popup.isShowing())
+            popup.show(LoginMenu.stage);
     }
 
     public void muteSound(MouseEvent mouseEvent) {
         SettingController.setMute(!SettingController.isMute());
-        if (SettingController.isMute()) GameMenu.mediaPlayer.pause();
-        else GameMenu.mediaPlayer.play();
+        if (SettingController.isMute()) mediaPlayer.pause();
+        else mediaPlayer.play();
     }
 
     public void restartGame(MouseEvent mouseEvent) throws Exception{
-        GameMenu.mediaPlayer.stop();
+        mediaPlayer.stop();
         new GameMenu().start(LoginMenu.stage);
     }
 
     public void exitGame(MouseEvent mouseEvent) throws Exception{
-        GameMenu.mediaPlayer.stop();
+        mediaPlayer.stop();
         new MainMenu().start(LoginMenu.stage);
+    }
+
+    public void nextSong(MouseEvent mouseEvent) {
+        GameMenu.currentMediaIndex = (GameMenu.currentMediaIndex + 1) % GameMenu.mediaList.size();
+        playMedia(GameMenu.mediaList.get(GameMenu.currentMediaIndex));
+    }
+
+    public void playMedia(Media media) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.dispose();
+        }
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
     }
 }
